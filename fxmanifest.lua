1f8c2f32e0da4158eba6c25b0959c6ac503d8575
Madsen <76999316+imMadsen@users.noreply.github.com>
2023-10-27 08:00:27 +0200

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+
+author "Rasmus Madsen"
+version '1.0.0'
+lua54 'yes'
+
+games {
+  "gta5",
+  "rdr3"
+}
+
+server_script "server.lua"
---
+++ server.lua
@@ -0,0 +1,60 @@
+exports("getMemberFromGroupByCitizenId", function(...) exports["imm-groups"]:getMemberFromGroup(...) end)
+exports("addMemberToGroupByCitizenId", function(...) exports["imm-groups"]:addMemberToGroup(...) end)
+exports("removeMemberFromGroupByCitizenId", function(...) exports["imm-groups"]:removeMemberFromGroup(...) end)
+exports("getMemberStateByCitizenId", function(...) exports["imm-groups"]:getMemberState(...) end)
+exports("setMemberStateByCitizenId", function(...) exports["imm-groups"]:setMemberState(...) end)
+exports("getGroupsFromMemberByCitizenId", function(...) exports["imm-groups"]:getGroupsFromMember(...) end)
+
+exports("getMemberFromGroupBySource", function(groupId, source)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:getMemberFromGroup(groupId, player.PlayerData.citizenid)
+end)
+
+exports("addMemberToGroupBySource", function(groupId, source)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:getMemberFromGroup(groupId, player.PlayerData.citizenid)
+end)
+
+exports("removeMemberFromGroupBySource", function(groupId, source)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:removeMemberFromGroup(groupId, player.PlayerData.citizenid)
+end)
+
+exports("getMemberStateBySource", function(groupId, source, key)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:getMemberState(groupId, player.PlayerData.citizenid, key)
+end)
+
+exports("setMemberStateBySource", function(groupId, source, key, value)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:setMemberStateBySource(groupId, player.PlayerData.citizenid, key, value)
+end)
+
+exports("getGroupsFromMemberBySource", function(source)
+    local player = QBCore.Functions.GetPlayer(source)
+    if (not player) then
+        return false
+    end
+
+    return exports["imm-groups"]:getGroupsFromMember(groupId, player.PlayerData.citizenid, key, value)
+end)
